264a05272319b9f36563b2f8a9080af06de6253d
Manu <manu@Mac-mini-de-Manu.local>
2025-10-14 15:39:58 +0200

    Initial commit

---
+++ src/test/java/com/paymybuddy/PaymybuddyV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddyV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
